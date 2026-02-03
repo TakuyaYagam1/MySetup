6d5634915d08c5b612697a5bcd35aa09b0b50b07
takuya <skr1ms13666@gmail.com>
2026-02-03 13:02:48 +0300

    update hardware configuration for intel and amd

--- Linux/NixOS/system/hardware.nix
+++ Linux/NixOS/system/hardware.nix
@@ -8,11 +8,19 @@
     enable32Bit = true;
     extraPackages = with pkgs; [
       intel-media-driver
+      intel-vaapi-driver
+      vaapiIntel
+      amdvlk
+      vaapiVdpau
       libvdpau-va-gl
+      mesa.drivers
     ];
   };
 
-  services.xserver.videoDrivers = [ "amdgpu" "modesetting" ];
+  services.xserver.videoDrivers = lib.mkDefault [ "amdgpu" "modesetting" ];
+
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   hardware.bluetooth = {
     enable = true;
