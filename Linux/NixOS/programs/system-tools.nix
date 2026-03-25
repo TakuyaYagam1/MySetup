bee45ef042c66b6947037504edc4de33766d3429
takuya <skr1ms13666@gmail.com>
2026-03-25 20:03:38 +0300

    rollback to stable version of amnezia-vpn

--- Linux/NixOS/programs/system-tools.nix
+++ Linux/NixOS/programs/system-tools.nix
@@ -1,11 +1,16 @@
-{ config, pkgs, ... }:
+{ config, pkgs, pkgs-stable, ... }:
 
 {
-  programs.amnezia-vpn.enable = true;
+  programs.amnezia-vpn = {
+    enable = true;
+    package = pkgs-stable.amnezia-vpn;
+  };
 
   boot.extraModulePackages = [ config.boot.kernelPackages.amneziawg ];
-  
-  environment.systemPackages = [ pkgs.amneziawg-tools ];
+
+  environment.systemPackages = [
+    pkgs-stable.amneziawg-tools
+  ];
 
   programs.dconf.enable = true;
 }
