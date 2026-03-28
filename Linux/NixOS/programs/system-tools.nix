2392dc0674ad9fa3d68581aaa12007c759758d25
takuya <skr1ms13666@gmail.com>
2026-03-28 21:54:19 +0300

    update amneziavpn to use x11

--- Linux/NixOS/programs/system-tools.nix
+++ Linux/NixOS/programs/system-tools.nix
@@ -1,16 +1,33 @@
-{ config, pkgs, pkgs-stable, ... }:
+{ config, pkgs, lib, ... }:
 
+let
+  amnezia-vpn-x11 = pkgs.symlinkJoin {
+    name = "amnezia-vpn-xwayland";
+    paths = [ pkgs.amnezia-vpn ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/AmneziaVPN \
+        --set QT_QPA_PLATFORM xcb
+        
+      if [ -f $out/share/applications/AmneziaVPN.desktop ]; then
+        sed -i 's|^Exec=.*|Exec='$out'/bin/AmneziaVPN|g' $out/share/applications/AmneziaVPN.desktop
+      fi
+    '';
+  };
+in
 {
   programs.amnezia-vpn = {
     enable = true;
-    package = pkgs-stable.amnezia-vpn;
+    package = amnezia-vpn-x11;
   };
 
-  boot.extraModulePackages = [ config.boot.kernelPackages.amneziawg ];
-
   environment.systemPackages = [
-    pkgs-stable.amneziawg-tools
+    pkgs.amneziawg-tools
   ];
 
+  boot.extraModulePackages = [ config.boot.kernelPackages.amneziawg ];
+  boot.kernelModules = [ "amneziawg" ];
   programs.dconf.enable = true;
+
+  networking.firewall.checkReversePath = lib.mkForce "loose";
 }
