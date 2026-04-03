79dc2a88640dd106f26cf4d5768b4da07844815e
takuya <skr1ms13666@gmail.com>
2026-04-03 15:22:36 +0300

    add swappy

--- Linux/NixOS/home/theming.nix
+++ Linux/NixOS/home/theming.nix
@@ -56,4 +56,11 @@
   home.sessionVariables = {
     XDG_DATA_DIRS = "$HOME/.nix-profile/share:/run/current-system/sw/share:$XDG_DATA_DIRS";
   };
+
+  xdg.configFile."swappy/config".text = ''
+    [Default]
+    save_dir=$HOME/Pictures/Screenshots
+    save_filename_format=screenshot-%Y%m%d-%H%M%S.png
+    show_panel=true
+  '';
 }
