264769c7f3728d8759071ad5e982f7187395b8d6
takuya <skr1ms13666@gmail.com>
2026-05-28 20:55:24 +0300

    fix(hypr): format rules for stylua

--- Linux/dots/hypr/hyprland/rules.lua
+++ Linux/dots/hypr/hyprland/rules.lua
@@ -35,7 +35,9 @@ end
 
 hl.window_rule({ match = { class = "btop" }, workspace = "special:sysmon" })
 hl.window_rule({
-	match = { class = "(?i)(feishin|spotify|supersonic|cider|com\\.github\\.th_ch\\.youtube_music|plexamp|com-maxrave-simpmusic-mainkt)" },
+	match = {
+		class = "(?i)(feishin|spotify|supersonic|cider|com\\.github\\.th_ch\\.youtube_music|plexamp|com-maxrave-simpmusic-mainkt)",
+	},
 	workspace = "special:music",
 })
 hl.window_rule({ match = { initial_title = "Spotify( Free)?" }, workspace = "special:music" })
