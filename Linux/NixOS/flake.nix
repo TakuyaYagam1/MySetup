d98912a86c18694885fed3b0fece6a8ba2b5d384
Takuya Yagami <skr1ms13666@gmail.com>
2026-07-28 01:41:48 +0000

    chore(nix): update noctalia (#89)

--- Linux/NixOS/flake.nix
+++ Linux/NixOS/flake.nix
@@ -28,7 +28,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia = {
-      url = "github:noctalia-dev/noctalia/v5.0.0-beta.5";
+      url = "github:noctalia-dev/noctalia/v5.0.0-beta.6";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia-shell = {
