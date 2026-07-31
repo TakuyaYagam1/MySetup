138bc04a6484544aeb4fe7622ac46724048157e7
TakuyaYagam1 <145888123+TakuyaYagam1@users.noreply.github.com>
2026-07-31 01:51:38 +0000

    chore(nix): update noctalia

--- Linux/NixOS/presets/desktop/flake.nix
+++ Linux/NixOS/presets/desktop/flake.nix
@@ -48,7 +48,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia = {
-      url = "github:noctalia-dev/noctalia/v5.0.0-beta.6";
+      url = "github:noctalia-dev/noctalia/v5.0.0-beta.7";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia-shell = {
