685718ca4f39220da947852595a3e88c0881e1ef
Takuya Yagami <skr1ms13666@gmail.com>
2026-07-31 01:56:45 +0000

    chore(nix): update noctalia (#93)

--- Linux/NixOS/lib/preset-inputs.nix
+++ Linux/NixOS/lib/preset-inputs.nix
@@ -62,7 +62,7 @@ let
       inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia = {
-      url = "github:noctalia-dev/noctalia/v5.0.0-beta.6";
+      url = "github:noctalia-dev/noctalia/v5.0.0-beta.7";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia-shell = {
--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia = {
-      url = "github:noctalia-dev/noctalia/v5.0.0-beta.6";
+      url = "github:noctalia-dev/noctalia/v5.0.0-beta.7";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia-shell = {
