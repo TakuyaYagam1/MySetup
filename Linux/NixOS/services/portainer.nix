cbe0e485fe7f8158b847f76880833227133964db
Takuya Yagami <skr1ms13666@gmail.com>
2026-08-13 04:20:34 +0300

    chore(nix): update portainer (#144)

--- Linux/NixOS/services/portainer.nix
+++ Linux/NixOS/services/portainer.nix
@@ -19,7 +19,7 @@ in
         backend = "docker";
 
         containers.portainer = {
-          image = "portainer/portainer-ce:2.39.5";
+          image = "portainer/portainer-ce:2.39.6";
           autoStart = true;
           ports = [
             "127.0.0.1:9443:9443"
