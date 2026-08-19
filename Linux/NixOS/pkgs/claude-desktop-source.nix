d6fa0dae2736d8cdf4909ace529720ad0e626048
TakuyaYagam1 <145888123+TakuyaYagam1@users.noreply.github.com>
2026-08-19 04:55:14 +0000

    chore(nix): update claude desktop

--- Linux/NixOS/pkgs/claude-desktop-source.nix
+++ Linux/NixOS/pkgs/claude-desktop-source.nix
@@ -1,10 +1,10 @@
 {
-  version = "1.32352.0";
+  version = "1.32885.1";
 
   sources = {
     x86_64-linux = {
-      url = "https://downloads.claude.ai/claude-desktop/apt/stable/pool/main/c/claude-desktop/claude-desktop_1.32352.0_amd64.deb";
-      hash = "sha256-JEDQdDdlU9cLIvweUzR9pg5YdXSYll7+uYmYh9ph4Nk=";
+      url = "https://downloads.claude.ai/claude-desktop/apt/stable/pool/main/c/claude-desktop/claude-desktop_1.32885.1_amd64.deb";
+      hash = "sha256-+KXd6nyMvnaVic8ZwuGDLV1TKrGb+CAmIbqVfJNRovw=";
     };
   };
 }
