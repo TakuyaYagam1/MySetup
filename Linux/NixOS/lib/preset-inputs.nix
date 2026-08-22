691bc1eb020fe070f4f8299d5ca15b0426367e65
Takuya Yagami <skr1ms13666@gmail.com>
2026-08-22 04:03:50 +0300

    chore(nix): update noctalia (#187)

--- Linux/NixOS/lib/preset-inputs.nix
+++ Linux/NixOS/lib/preset-inputs.nix
@@ -67,7 +67,7 @@ let
       inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia = {
-      url = "github:noctalia-dev/noctalia/v5.0.0-beta.8";
+      url = "github:noctalia-dev/noctalia/v5.0.0-beta.9";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia-shell = {
